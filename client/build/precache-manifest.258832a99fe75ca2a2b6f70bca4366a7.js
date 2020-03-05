6bfad41d54e0c9f00330c5a2617807c9fafd5026
Johnny Burke <johnalbertburke@gmail.com>
2020-03-05 21:30:54 +0100

    run build

---
+++ client/build/precache-manifest.258832a99fe75ca2a2b6f70bca4366a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e6017015311c260ea73f61e0477daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c58dde7216ffca958786",
+    "url": "/static/css/main.b97425a3.chunk.css"
+  },
+  {
+    "revision": "5cd0270e84bec3e62392",
+    "url": "/static/js/2.6e950d22.chunk.js"
+  },
+  {
+    "revision": "7016091ca55dbc0d622cab2ce85b90d1",
+    "url": "/static/js/2.6e950d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58dde7216ffca958786",
+    "url": "/static/js/main.e6c21920.chunk.js"
+  },
+  {
+    "revision": "a2260f3d34681268f31e",
+    "url": "/static/js/runtime-main.f077d572.js"
+  }
+]);
