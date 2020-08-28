954327b303731e6713445d973cd4ef500cbd143d
Johnny Burke <johnalbertburke@gmail.com>
2020-08-28 11:07:02 -0700

    toubleshoot npm run build bug

---
+++ client/build/precache-manifest.2220043672de0c6f59f76c57a87c041a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1201cdcf7734dd83a789ad5d3c301b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84247e57767eb6e2379",
+    "url": "/static/css/main.abd80ecf.chunk.css"
+  },
+  {
+    "revision": "5cd0270e84bec3e62392",
+    "url": "/static/js/2.6e950d22.chunk.js"
+  },
+  {
+    "revision": "7016091ca55dbc0d622cab2ce85b90d1",
+    "url": "/static/js/2.6e950d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84247e57767eb6e2379",
+    "url": "/static/js/main.813f6334.chunk.js"
+  },
+  {
+    "revision": "a2260f3d34681268f31e",
+    "url": "/static/js/runtime-main.f077d572.js"
+  }
+]);
