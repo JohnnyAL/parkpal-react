f5d561c2d8039256f07ab9aac2e43b8f7661f35e
Johnny Burke <johnalbertburke@gmail.com>
2020-08-27 17:45:03 -0700

    update background image positioning

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4deec7c3295dfa5a35aefaff01963cad.js"
+  "/precache-manifest.e81347e9e413309c071200590c96e6b5.js"
 );
 
 self.addEventListener('message', (event) => {
